a2b9bdac14916bb5f662eaeb3dbf96674117fb24
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-06-01 15:19:30 +0300

    Initial Commit

---
+++ FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwiftUIApp.swift
+//  FirstAppSwiftUI
+//
+//  Created by Meltem Uğurlu on 1.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
